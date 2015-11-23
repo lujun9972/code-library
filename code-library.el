45928d61d373e987c617ac743076cbb946356078
lujun9972 <lujun9972@gmail.com>
2015-11-23 22:58:26 +0800

    add comments

--- code-library.el
+++ code-library.el
@@ -1,8 +1,41 @@
-;; 记录代码片段到特定文件中
-(require 'file-helper)
-(require 'buffer-helper)
+;;; code-library.el --- use org-mode to manage the code blocks
+
+;; Copyright (C) 2004-2015 Free Software Foundation, Inc.
+
+;; Author: DarkSun <lujun9972@gmail.com>
+;; Created: 2015-11-03
+;; Version: 0.1
+;; Keywords: lisp, code
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Source code
+;;
+;; typing-game's code can be found here:
+;;   http://github.com/lujun9972/code-library
+
+;;; Commentary:
+
+;; code-library is a tool that use org-mode to manager code blocks.
+
+;;; Code:
+
 (defgroup code-library-group nil
-  "代码库设置")
+  "code library group"
+  :prefix "code-library-")
 (defcustom code-library-mode-file-alist '((c++-mode . "cpp.org")
 										  (emacs-lisp-mode . "elisp.org")
 										  (python-mode . "python.org")
@@ -13,10 +46,13 @@
 
 (defcustom code-library-path "~/CodeLibrary/"
   "代码库文件存储的目录")
+
 (defun code-library-save-code()
   "保存所选代码到代码库文件中"
   (interactive)
-  (let* ((code (get-region-or-thing 'defun))
+  (let* ((code (if (region-active-p)
+                   (buffer-substring-no-properties (region-beginning) (region-end))
+                 (thing-at-point 'defun)))
 		 (library-base-file (or (cdr (assoc major-mode code-library-mode-file-alist))
 								"temp.org"))
 		 (library-file (concat code-library-path library-base-file))
